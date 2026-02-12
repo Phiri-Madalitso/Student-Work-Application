fad73657c4eb64a3fc6029770f90b55df93179e4
JabirMAG <Jabirgitale@gmail.com>
2026-02-12 13:37:25 +0100

    Endring

---
+++ Model/Student.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, student_id: id, name: str, age: int, year: int, skills: list):
+        self.student_id = student_id
+        self.name = name
+        self.age = age
+        self.year = year
+        self.skills = skills
+
+    def __repr__(self):
+        return f"Student({self.student_id}, {self.name}, {self.age}, {self.year}, {self.skills})"
+
+
+stud1 = Student(2800, "Thom", 23, 2, ["Python", "Java", "Github"])
+
+print(stud1)

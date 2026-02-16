533d58f874dcad2319f047163e19e26b741c7f7f
JabirMAG <Jabirgitale@gmail.com>
2026-02-16 13:42:57 +0100

    Refactor Student class to include email attribute and update representation; add Company and Jobs classes with initial implementations.

---
+++ Model/Company.py
@@ -0,0 +1,11 @@
+class Company:
+    def __init__(self, company_id: int, name: str, email = str):
+        self.company_id = company_id,
+        self.name = name,
+        self.email = email
+
+    def __repr__(self):
+        return f"Company({self.company_id}, {self.name}, {self.email})"
+        
+comp1 = Company(101, "Tech Solutions Inc.", "contact@techsolutions.com")
+print(comp1)        
--- Model/Student.py
+++ Model/Student.py
@@ -1,15 +1,16 @@
 class Student:
-    def __init__(self, student_id: id, name: str, age: int, year: int, skills: list):
+    def __init__(self, student_id: int, name: str, email: str, age: int, year: int, skills: list):
         self.student_id = student_id
         self.name = name
+        self.email = email
         self.age = age
         self.year = year
         self.skills = skills
 
     def __repr__(self):
-        return f"Student({self.student_id}, {self.name}, {self.age}, {self.year}, {self.skills})"
+        return f"Student({self.student_id}, {self.name}, {self.email}, {self.age}, {self.year}, {self.skills})"
 
 
-stud1 = Student(2880, "Thom", 23, 2, ["Python", "Java", "Github", "Git", "Agile"])
+stud1 = Student(2880, "Thom", "Thom@Gmail.Com", 23, 2, ["Python", "Java", "Github", "Git", "Agile"])
 
 print(stud1)
